68878d09f94a62661720574a17f7b522ae838e2b
Jelmer Prins <jelmer@sumocoders.be>
2019-11-14 08:23:43 +0100

    Generated initial code for the FAQ module

---
+++ src/Backend/Modules/Faq/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Backend\Modules\Faq;
+
+use Backend\Core\Engine\Base\Config as BaseConfig;
+
+final class Config extends BaseConfig
+{
+}
---
+++ src/Frontend/Modules/Faq/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Frontend\Modules\Faq;
+
+use Frontend\Core\Engine\Base\Config as BaseConfig;
+
+final class Config extends BaseConfig
+{
+}
